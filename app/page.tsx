c33606490f670be4c9508fb8fa7128e4a277950f
Kenrick Wu <kenrick.wu@gmail.com>
2025-08-05 20:15:08 +0800

    Added some basic colour and sizing variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
+import Introduction from "@/components/Home/Introduction";
+
 export default function Home() {
   return (
-    <article>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi accusantium
-      atque fuga voluptates. Quis maxime nemo rem quaerat molestiae ullam! Fuga
-      exercitationem aut sapiente quo adipisci tempora numquam unde veniam. Ex
-      maiores quisquam expedita veritatis autem molestias omnis porro nostrum
-      sapiente quidem quo harum odit ipsam vitae cupiditate perspiciatis
-      obcaecati, dolore blanditiis sed quae? Nulla quia adipisci odit ipsum
-      cumque!
-    </article>
+    <main>
+      <h1>Welcome to My Portfolio</h1>
+      <Introduction />
+    </main>
   );
 }
---
+++ components/Home/Introduction.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Introduction() {
+  return (
+    <div className="container">
+      Introduction to my portfolio. Here you will find information about my
+      work, skills, and interests. I am passionate about creating impactful
+      projects that make a difference in the world. Feel free to explore my
+      projects and get in touch if you have any questions or opportunities for
+      collaboration.
+      <p>
+        I specialize in web development, design, and user experience. My goal is
+        to create intuitive and engaging digital experiences that resonate with
+        users. I believe in the power of technology to transform ideas into
+        reality.
+      </p>
+      <p>
+        Thank you for visiting my portfolio. I look forward to connecting with
+        you!
+      </p>
+    </div>
+  );
+}
