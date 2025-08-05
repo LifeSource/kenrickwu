317989aa8d1ac3a9af451adb07d617a8a52a0f9a
Kenrick Wu <kenrick.wu@gmail.com>
2025-08-05 14:06:20 +0800

    Added a basic about page

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AboutMe() {
+  return (
+    <div>
+      Hi I'm a down to earth person who loves the creative side of things.
+    </div>
+  );
+}
