ce60fc28e71e18783861bbef04c371b81a109310
Kenrick Wu <kenrick.wu@gmail.com>
2025-08-05 14:07:23 +0800

    Fixed up quote issue

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AboutMe() {
   return (
     <div>
-      Hi I'm a down to earth person who loves the creative side of things.
+      Hi I&quote;m a down to earth person who loves the creative side of things.
     </div>
   );
 }
